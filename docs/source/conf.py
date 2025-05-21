2b99535fd48bc3591d2615145c4fdf99bbfc5c87
jwa7 <jwa7@iu.edu>
2025-05-21 02:00:49 -0400

    update docs to try debugging the svg embedding in RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,6 @@ extensions = [
     'nbsphinx',                  # Jupyter notebook integration
     'sphinx_copybutton',         # Add "copy" button to code blocks
     'sphinx_autodoc_typehints',  # Use Python type hints in documentation
-    'sphinxcontrib_svg2pdfconverter',
 ]
 
 # Add any paths that contain templates here
